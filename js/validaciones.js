96bff927f1220450e96b79a4f0c864c2b3de01e8
DiegoLopez01 <di.lopezp@duocuc.cl>
2022-05-01 23:59:04 -0400

    agregarApi_cambiosform

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,25 +1,39 @@
-function validarCorreo (email){
-	re=/^([admin]+)@(gmail]+)\.([com]{2,6})$/
-	if(!re.exec(email)){
-		alert('email no valido');
-        
-	}
-	else alert('email valido');
-	}
-	var url = "https://apis.digital.gob.cl/dpa/regiones";
-	fetch(url).then(function(result){
-		if(result.ok){
-			return result.json
-		}
-  
-  	}).then(function(data){
-	    data.forEach(function(element){
-			let regiones = document.getElementById("regiones");
-		  	let opt = document.createElement("option");
-		  	opt.appendChild(document.createTextNode(element.nombre));
-		  	opt.value=element.codigo
 
-		  	regiones.appendChild(opt)
-		  
-	  	});
-  	})
+/*----API_REGIONES----*/ 
+var url = "https://apis.digital.gob.cl/dpa/regiones";
+
+fetch(url).then(function (result) {
+	if (result.ok) {
+		return result.json();
+	}
+
+}).then(function (data) {
+	data.forEach(function (element) {
+		let regiones = document.getElementById("regiones");
+		let opt = document.createElement("option");
+		opt.appendChild(document.createTextNode(element.nombre));
+		opt.value = element.codigo;
+
+		regiones.appendChild(opt);
+
+	})
+})
+/*----API_COMUNAS----*/ 
+var url = "https://apis.digital.gob.cl/dpa/comunas";
+
+fetch(url).then(function (result) {
+	if (result.ok) {
+		return result.json();
+	}
+
+}).then(function (data) {
+	data.forEach(function (element) {
+		let comunas = document.getElementById("comunas");
+		let opt = document.createElement("option");
+		opt.appendChild(document.createTextNode(element.nombre));
+		opt.value = element.codigo;
+
+		comunas.appendChild(opt);
+
+	})
+})
