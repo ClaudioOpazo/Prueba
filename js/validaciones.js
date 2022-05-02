145784c7500692ac888c2b267b1ed0cd14e76ca6
DiegoLopez01 <di.lopezp@duocuc.cl>
2022-05-01 22:30:18 -0400

    api_alerta

--- js/validaciones.js
+++ js/validaciones.js
@@ -6,3 +6,20 @@ function validarCorreo (email){
 	}
 	else alert('email valido');
 	}
+	var url = "https://apis.digital.gob.cl/dpa/regiones";
+	fetch(url).then(function(result){
+		if(result.ok){
+			return result.json
+		}
+  
+  	}).then(function(data){
+	    data.forEach(function(element){
+			let regiones = document.getElementById("regiones");
+		  	let opt = document.createElement("option");
+		  	opt.appendChild(document.createTextNode(element.nombre));
+		  	opt.value=element.codigo
+
+		  	regiones.appendChild(opt)
+		  
+	  	});
+  	})
